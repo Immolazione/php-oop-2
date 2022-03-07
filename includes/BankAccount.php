7cde64436243ee15ef729e32954e330aef83e5a1
Immolazione <federico.deered@gmail.com>
2022-03-07 03:11:28 +0100

    add bankAccount

---
+++ includes/BankAccount.php
@@ -0,0 +1,28 @@
+<?php
+
+trait BankAccount {
+    public $account_number;
+    public $balance;
+
+    //SETTERs
+    public function setAccountNumber($account_number) {
+        $this->account_number = $account_number;
+        return $this;
+    }
+
+    public function setBalance($balance) {
+        $this->balance = $balance;
+        return $this;
+    }
+
+    //GETTERs
+    public function getAccountNumber($account_number) {
+        return $this->account_number;
+    }
+
+    public function getBalance($balance) {
+        return $this->balance;
+    }
+}
+
+?>
